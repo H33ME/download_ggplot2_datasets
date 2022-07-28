85b82c06b6d05180346c4883a5fb782a4ac70a8d
Herman N <96638588+H33ME@users.noreply.github.com>
2022-07-28 15:31:22 +0300

    Add files via upload
    This is a shiny app that downloads ggplot2  datasets. 
    This files downloaded can be saved in the computer.
    The files are written as .tsv file type.

---
+++ app.R
@@ -0,0 +1,36 @@
+#### download datasets from the ggplot packages using shiny App
+
+library(ggplot2)
+library(shiny)
+
+ui<- fluidPage(
+    selectInput("dataset","Dataset",choices=data(package="ggplot2")$result[,"Item"]),
+    tableOutput("sneak_peak"),
+    downloadButton("download","Download ggplot2 .tsv file")
+)
+
+server<- function(input,output,session){
+    dat<- reactive({
+        out<- get(input$dataset,"package:ggplot2")
+        
+        if(!is.data.frame(out)){
+            validate("'", input$dataset, "' is not a dataframe thus cannot be downloaded")
+        }
+        out
+    })
+    output$sneak_peak<- renderTable({
+        head(dat())
+    })
+    output$download<- downloadHandler(
+        filename = function(){
+            paste0(input$dataset,".tsv")
+        },
+        content = function(file){
+            vroom::vroom_write(dat(),file)
+        }
+    )
+}
+
+shinyApp(ui,server)
+
+getwd()
